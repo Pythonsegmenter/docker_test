2e4618e7d6813762ac3e2ed085f0facd9cb56f89
aspyro-coder <jeroen.vancraen@aspyro.be>
2022-11-24 20:33:13 -0300

    remove adhoc

--- run.py
+++ run.py
@@ -1,7 +1,27 @@
 from flask import Flask
 
-app = Flask(__name__)
+class ReverseProxied(object):
+    """
+    Because we are reverse proxied from an aws load balancer
+    use environ/config to signal https
+    since flask ignores preferred_url_scheme in url_for calls
+    """
+    def __init__(self, app):
+        self.app = app
 
+    def __call__(self, environ, start_response):
+        # if one of x_forwarded or preferred_url is https, prefer it.
+        forwarded_scheme = environ.get("HTTP_X_FORWARDED_PROTO", None)
+        preferred_scheme = app.config.get("PREFERRED_URL_SCHEME", None)
+        if "https" in [forwarded_scheme, preferred_scheme]:
+            environ["wsgi.url_scheme"] = "https"
+        return self.app(environ, start_response)
+
+
+app = Flask(__name__)
+# set to always use https instead of http
+app.config.update(dict(PREFERRED_URL_SCHEME='https'))
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
